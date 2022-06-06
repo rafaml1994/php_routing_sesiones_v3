1b1d6c3e441e5c1252457a92373b21c0ea7bfda9
rafa <rafa_ml_smv@hotmail.com>
2022-06-06 13:50:01 +0200

    commit_06_06_2022

--- connection.php
+++ connection.php
@@ -4,12 +4,12 @@ class Connect
 {
     public static function getConnection()
     {
-        //Conexión bd nube:
-        //$conexion = new PDO("pgsql:host=bj3tedylayej2naoqaso-postgresql.services.clever-cloud.com;port=5432;dbname=bj3tedylayej2naoqaso", "ugqnfhoj1rro5xqz0w7f", "SSg1p5HbN2LhLtJvtJeS");
-        //return $conexion;
+        //Conexión bd nube: (si hacemos el commit con la conexion comentada, se actualiza y no funciona en internet) 
+        $conexion = new PDO("pgsql:host=bj3tedylayej2naoqaso-postgresql.services.clever-cloud.com;port=5432;dbname=bj3tedylayej2naoqaso", "ugqnfhoj1rro5xqz0w7f", "SSg1p5HbN2LhLtJvtJeS");
+        return $conexion;
 
         //Conexión con servidor en Docker:
-        $conexion = new PDO("pgsql:host=postgres;port=5432;dbname=pruebas", "admin", "admin");
-        return $conexion;
+        //$conexion = new PDO("pgsql:host=postgres;port=5432;dbname=pruebas", "admin", "admin");
+        //return $conexion;
     }
 }
